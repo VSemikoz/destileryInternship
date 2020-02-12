2140fb08da9014bf4822eaf8029d4e5e39b60147
VSemikoz <slava.semik@yandex.ru>
2020-02-12 12:20:54 +0300

    add news feed fragment

---
+++ app/src/main/java/ru/vssemikoz/newsfeed/fragments/newsFeedFragment.java
@@ -0,0 +1,30 @@
+package ru.vssemikoz.newsfeed.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.vssemikoz.newsfeed.R;
+
+public class newsFeedFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
