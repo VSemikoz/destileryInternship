6ce4b63a8827015514774b5039115640cd1a4ff6
VSemikoz <slava.semik@yandex.ru>
2020-02-12 12:36:18 +0300

    change layouts to fragment model:
     - change activity main layout to fragment carrier
     - add news_feed_fragment as fragment
     - inflate fragment by news_feed_fragment

--- app/src/main/java/ru/vssemikoz/newsfeed/fragments/newsFeedFragment.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/fragments/newsFeedFragment.java
@@ -20,7 +20,7 @@ public class newsFeedFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.news_feed_fragment, container, false);
     }
 
     @Override
