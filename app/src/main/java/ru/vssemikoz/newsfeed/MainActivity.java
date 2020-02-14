bfae3e7a9be21d50ff5e5b205a57b96a521cc729
VSemikoz <slava.semik@yandex.ru>
2020-02-14 19:09:02 +0300

    config Activity to get date from api:  - Retrofit initialization  - delete initData func  - takeaway RecView initialization into func

--- app/src/main/java/ru/vssemikoz/newsfeed/MainActivity.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/MainActivity.java
@@ -5,34 +5,63 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import ru.vssemikoz.newsfeed.adapters.NewsFeedAdapter;
+import ru.vssemikoz.newsfeed.api.NewsApi;
 import ru.vssemikoz.newsfeed.models.NewsItem;
+import ru.vssemikoz.newsfeed.models.NewsItemList;
 
 public class MainActivity extends AppCompatActivity {
-
+    String KEY = "c94a57cbbb50497f94a2bb167dc91fc5";
     List<NewsItem> newsItems = new ArrayList<NewsItem>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initData();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        NewsApi newsApi = retrofit.create(NewsApi.class);
+        Call<NewsItemList> call = newsApi.getNews("ru", KEY);
+
+        call.enqueue(new Callback<NewsItemList> () {
+            @Override
+            public void onResponse(Call<NewsItemList>  call, Response<NewsItemList>  response) {
+                if (!response.isSuccessful()){
+                    Log.d("MyLog", "onResponse " + response.code());
+                    return;
+                }
+                newsItems = response.body().getNewsItem();
+                Log.d("MyLog", "onSuccess " + newsItems);
+                initRecView();
+            }
+
+            @Override
+            public void onFailure(Call<NewsItemList>  call, Throwable t) {
+                Log.d("MyLog", "onFailure " + Objects.requireNonNull(t.getMessage()));
+            }
+        });
+
+    }
+
+    void initRecView(){
         RecyclerView recyclerView =  findViewById(R.id.rv_news_feed);
         NewsFeedAdapter adapter = new NewsFeedAdapter(newsItems);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
 
-    private void initData(){
-        newsItems.add(new NewsItem("Экс-глава управления ФСИН попытался покончить с собой в суде после приговора по делу о вымогательстве - Новая газета",
-                "Экс-глава управления ФСИН Виктор Свиридов попытался застрелиться в Чертановском суде Москвы во время оглашения приговора по делу о вымогательстве (ч. 3 ст. 163 УК). Об этом сообщает РБК. \\r\\n\\r\\nСвиридов до приговора был под подпиской о невыезде. После слов судьи…"));
-        newsItems.add(new NewsItem("Польша запросила у 'Газпрома' данные для дела против Nord Stream 2 - РИА НОВОСТИ",
-                "Польский комитет конкуренции и защиты потребителей (UOKiK) запросил информацию у \\\"Газпрома\\\" для проведения расследования в отношении проекта газопровода... РИА Новости, 12.02.2020"));
-        newsItems.add(new NewsItem("Рассекречена дешевая версия AirPods - Lenta.ru", "Apple планирует выпустить дешевую версию AirPods Pro. Об этом говорится в отчете знакомого с цепочкой поставок источника. В сообщении говорится, что компания намерена увеличить количество поставок девайсов iPad, Apple Watch, iMac и AirPods Pro Lite, чтобы удо…"));
-        newsItems.add(new NewsItem("Microsoft выпустила эмулятор Windows 10X. Опробовать новую ОС могут все желающие - 3DNews", "Microsoft выпустила эмулятор операционной системы Windows 10X."));
-    }
 }
