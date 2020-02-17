aba04a2e0406b62cf941a835ed44e21ad18a64f8
VSemikoz <slava.semik@yandex.ru>
2020-02-17 16:40:28 +0300

    Change DataBase version cause DAO model have been changed

--- app/src/main/java/ru/vssemikoz/newsfeed/DataBase/NewsAppDataBase.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/DataBase/NewsAppDataBase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import ru.vssemikoz.newsfeed.DAO.NewsItemDAO;
 import ru.vssemikoz.newsfeed.models.NewsItem;
 
-@Database(entities = NewsItem.class, version = 1)
+@Database(entities = {NewsItem.class}, version = 3)
 public abstract class NewsAppDataBase extends RoomDatabase {
     public abstract NewsItemDAO newsItemDAO();
 }
