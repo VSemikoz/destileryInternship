1c8f568846fabd0fa1853b14f9dabb1da73ae639
VSemikoz <slava.semik@yandex.ru>
2020-02-18 14:21:40 +0300

    rename NewsItemList to NewsApiResponse and NewsItem to NewsApiResponseItem

--- app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
@@ -1,14 +1,11 @@
 package ru.vssemikoz.newsfeed.api;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
-import ru.vssemikoz.newsfeed.models.NewsItem;
-import ru.vssemikoz.newsfeed.models.NewsItemList;
+import ru.vssemikoz.newsfeed.models.NewsApiResponse;
 
 public interface NewsApi {
     @GET("/v2/top-headlines")
-    Call<NewsItemList> getNews(@Query("country") String countryName, @Query("apiKey") String key);
+    Call<NewsApiResponse> getNews(@Query("country") String countryName, @Query("apiKey") String key);
 }
---
+++ app/src/main/java/ru/vssemikoz/newsfeed/models/NewsApiResponseItem.java
@@ -0,0 +1,79 @@
+package ru.vssemikoz.newsfeed.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+public class NewsApiResponseItem {
+    private String author;
+    private String title;
+    private String description;
+    private String content;
+    private String url;
+    @SerializedName("urlToImage")
+    private String imageUrl;
+    private Date publishedAt;
+
+    public NewsApiResponseItem(){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Date getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setPublishedAt(Date publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+}
+
+
