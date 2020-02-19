a907f4a7dc609356e6b551868e359027898416b6
VSemikoz <slava.semik@yandex.ru>
2020-02-19 19:07:05 +0300

    add Dialog to pick category

---
+++ app/src/main/java/ru/vssemikoz/newsfeed/dialogs/PickCategoryDialog.java
@@ -0,0 +1,44 @@
+package ru.vssemikoz.newsfeed.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import ru.vssemikoz.newsfeed.models.Category;
+import ru.vssemikoz.newsfeed.utils.Enum;
+
+public class PickCategoryDialog extends DialogFragment {
+    public interface NoticeDialogListener {
+        void onDialogSelectCategory(Category selectCategory);
+    }
+    private NoticeDialogListener nListener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        String[] categoryStrList = Enum.getCategoryNameList();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("PickCategory")
+                .setItems(categoryStrList, (dialog, which) -> {
+                    nListener.onDialogSelectCategory(Category.values()[which]);
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            nListener = (NoticeDialogListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+}
