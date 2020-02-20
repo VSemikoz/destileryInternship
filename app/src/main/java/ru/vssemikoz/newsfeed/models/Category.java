18749ed1346e13c91cb95c67c4169bc7a959d90e
VSemikoz <slava.semik@yandex.ru>
2020-02-20 15:54:05 +0300

    replace functionality from Enum -> Category

--- app/src/main/java/ru/vssemikoz/newsfeed/models/Category.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/models/Category.java
@@ -1,20 +1,51 @@
 package ru.vssemikoz.newsfeed.models;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import ru.vssemikoz.newsfeed.MainApplication;
+import ru.vssemikoz.newsfeed.R;
+
 public enum Category {
-    all("Все"),
-    business("Бизнес"),
-    entertainment("Развлечения"),
-    health("Здоровье"),
-    science("Наука"),
-    sport("Спорт"),
-    technology("Технологии");
 
-    private String strName;
-    Category(String stringCategory){
-        this.strName = stringCategory;
+    ALL(R.string.category_all),
+    BUSINESS(R.string.category_business),
+    ENTERTAINMENT(R.string.category_entertainment),
+    HEALTH(R.string.category_health),
+    SCIENCE(R.string.category_science),
+    SPORT(R.string.category_sport),
+    TECHNOLOGY(R.string.category_technology);
+
+    private Integer categoryId;
+
+    public static String getCategoryName(Category category){
+        return category.toString();
     }
 
-    public String getStrName() {
-        return strName;
+    Category(int stringCategory){
+        this.categoryId = stringCategory;
     }
+
+    @Override
+    public String toString() {
+        return MainApplication.getmContext().getString(categoryId);
+    }
+
+    public static String[] getCategoryNameList(){
+        Category[] categories = Category.values();
+        String[] categoriesName = new String[categories.length];
+
+        for (int i = 0; i < categories.length; i++){
+            categoriesName[i] = getCategoryName(categories[i]);
+        }
+        return categoriesName;
+    }
+
 }
