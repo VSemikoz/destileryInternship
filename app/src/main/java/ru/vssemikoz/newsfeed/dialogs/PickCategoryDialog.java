fa027717db6ce88a9c414c9c0bb5dc10655551b8
VSemikoz <slava.semik@yandex.ru>
2020-02-20 15:53:08 +0300

    rename var and interface

--- app/src/main/java/ru/vssemikoz/newsfeed/dialogs/PickCategoryDialog.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/dialogs/PickCategoryDialog.java
@@ -9,24 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import ru.vssemikoz.newsfeed.R;
 import ru.vssemikoz.newsfeed.models.Category;
-import ru.vssemikoz.newsfeed.utils.Enum;
 
 public class PickCategoryDialog extends DialogFragment {
-    public interface NoticeDialogListener {
-        void onDialogSelectCategory(Category selectCategory);
+    public interface OnCategorySelectedListener {
+        void onCategorySelected(Category selectCategory);
     }
-    private NoticeDialogListener nListener;
+    private OnCategorySelectedListener nListener;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        String[] categoryStrList = Enum.getCategoryNameList();
+        String[] categories = Category.getCategoryNameList();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Выбрать категрию")
-                .setItems(categoryStrList, (dialog, which) -> {
-                    nListener.onDialogSelectCategory(Category.values()[which]);
+        builder.setTitle(R.string.chose_category_title)
+                .setItems(categories, (dialog, which) -> {
+                    nListener.onCategorySelected(Category.values()[which]);
                 });
         return builder.create();
     }
@@ -35,10 +35,10 @@ public class PickCategoryDialog extends DialogFragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         try {
-            nListener = (NoticeDialogListener) getActivity();
+            nListener = (OnCategorySelectedListener) getActivity();
         } catch (ClassCastException e) {
             throw new ClassCastException(getActivity().toString()
-                    + " must implement NoticeDialogListener");
+                    + " must implement OnCategorySelectedListener");
         }
     }
 }
