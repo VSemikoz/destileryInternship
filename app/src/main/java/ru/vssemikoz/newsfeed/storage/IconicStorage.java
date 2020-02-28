f9b797aac47ce346da812e099273d7e795fb710f
VSemikoz <slava.semik@yandex.ru>
2020-02-28 15:22:20 +0300

    add IconicStorage

---
+++ app/src/main/java/ru/vssemikoz/newsfeed/storage/IconicStorage.java
@@ -0,0 +1,63 @@
+package ru.vssemikoz.newsfeed.storage;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+
+import com.mikepenz.iconics.IconicsColor;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.IconicsSize;
+import com.mikepenz.iconics.typeface.library.googlematerial.GoogleMaterial;
+
+public class IconicStorage {
+    private static Drawable STAR_YELLOW_BORDERLESS = null;
+    private static Drawable STAR_YELLOW_BORDER = null;
+    private static Drawable STAR_WHITE_BORDERLESS = null;
+    private static Drawable STAR_WHITE_BORDER = null;
+
+
+    public static Drawable getYellowStarBorderless(Context context) {
+        if (STAR_YELLOW_BORDERLESS == null) {
+            STAR_YELLOW_BORDERLESS = new IconicsDrawable(context)
+                    .icon(GoogleMaterial.Icon.gmd_star)
+                    .color(IconicsColor.colorInt(Color.YELLOW))
+                    .size(IconicsSize.dp(48));
+        }
+        return STAR_YELLOW_BORDERLESS;
+    }
+
+    public static Drawable getYellowStarBorder(Context context) {
+        if (STAR_YELLOW_BORDER == null) {
+            STAR_YELLOW_BORDER = new IconicsDrawable(context)
+                    .icon(GoogleMaterial.Icon.gmd_star)
+                    .color(IconicsColor.colorInt(Color.YELLOW))
+                    .size(IconicsSize.dp(48))
+                    .contourColor(IconicsColor.colorInt(Color.BLACK))
+                    .contourWidth(IconicsSize.dp(2));
+        }
+        return STAR_YELLOW_BORDER;
+    }
+
+    public static Drawable getWhiteStarBorderless(Context context) {
+        if (STAR_WHITE_BORDERLESS == null) {
+            STAR_WHITE_BORDERLESS = new IconicsDrawable(context)
+                    .icon(GoogleMaterial.Icon.gmd_star)
+                    .color(IconicsColor.colorInt(Color.WHITE))
+                    .size(IconicsSize.dp(48));
+        }
+        return STAR_WHITE_BORDERLESS;
+    }
+
+    public static Drawable getWhiteStarBorder(Context context) {
+        if (STAR_WHITE_BORDER == null) {
+            STAR_WHITE_BORDER = new IconicsDrawable(context)
+                    .icon(GoogleMaterial.Icon.gmd_star)
+                    .color(IconicsColor.colorInt(Color.WHITE))
+                    .size(IconicsSize.dp(48))
+                    .contourColor(IconicsColor.colorInt(Color.BLACK))
+                    .contourWidth(IconicsSize.dp(2));
+        }
+        return STAR_WHITE_BORDER;
+    }
+
+}
