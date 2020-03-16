93bbd4ee03c650e2a6ec027ca7585f42d7f01598
VSemikoz <slava.semik@yandex.ru>
2020-03-16 21:18:27 +0300

    contract methods implementation

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedFragment.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedFragment.java
@@ -2,6 +2,7 @@ package ru.vssemikoz.newsfeed.newsfeed;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,56 +12,68 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+import java.util.Objects;
+
 import ru.vssemikoz.newsfeed.R;
 import ru.vssemikoz.newsfeed.adapters.NewsFeedAdapter;
+import ru.vssemikoz.newsfeed.dialogs.PickCategoryDialog;
+import ru.vssemikoz.newsfeed.models.Category;
+import ru.vssemikoz.newsfeed.models.NewsItem;
 import ru.vssemikoz.newsfeed.storage.IconicStorage;
 
 import static androidx.core.util.Preconditions.checkNotNull;
 
 public class NewsFeedFragment extends Fragment implements NewsFeedContract.View {
-    private NewsFeedContract.Presenter mPresenter;
+    private String TAG = NewsFeedFragment.class.getName();
+
+    private NewsFeedContract.Presenter presenter;
     private Context context;
     private NewsFeedAdapter adapter;
     private RecyclerView recyclerView;
     private TextView emptyView;
     private ImageButton favoriteNewsButton;
     private ImageButton categoryButton;
-    private ProgressBar progressBar;
+    private ProgressBar progressBar;// TODO: 16.03.2020 implement progress bar on loads
     private TextView descriptionView;
 
     public NewsFeedFragment(){}
 
-    public static NewsFeedFragment newInstance() {
+    static NewsFeedFragment newInstance() {
         return new NewsFeedFragment();
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = getContext();
+
+        context = Objects.requireNonNull(getActivity()).getApplicationContext();
         adapter = new NewsFeedAdapter(context);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mPresenter.start();
+        updateCategoryNameOnDescription();
+        setFavoriteIcon();
+        presenter.start();
     }
 
     @Override
     public void setPresenter(NewsFeedContract.Presenter presenter) {
-        mPresenter = checkNotNull(presenter);
+        this.presenter = checkNotNull(presenter);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.news_feed_fragment, container, false);
+        View root = inflater.inflate(R.layout.newsfeed_frag, container, false);
 
         recyclerView = root.findViewById(R.id.rv_news_feed);
         emptyView = root.findViewById(R.id.tv_empty_view);
@@ -68,30 +81,54 @@ public class NewsFeedFragment extends Fragment implements NewsFeedContract.View 
         progressBar = root.findViewById(R.id.progress_bar);
         descriptionView = root.findViewById(R.id.tv_description);
         categoryButton = root.findViewById(R.id.ib_category);
+        initRecyclerView();
+        favoriteNewsButton.setOnClickListener(v -> {
+            presenter.invertFavoriteState();
+            setFavoriteIcon();
+            updateNews();
+            fillFragmentByView();
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        });
+        categoryButton.setOnClickListener(v -> {
+            DialogFragment categoryDialog = new PickCategoryDialog();
+            categoryDialog.show(Objects.requireNonNull(getActivity()).getSupportFragmentManager(),
+                    "categoryDialog");
+        });
+        return root;
+    }
+
+    private void initRecyclerView(){
         adapter = new NewsFeedAdapter(context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
         adapter.setOnItemClickListener(new NewsFeedAdapter.OnNewsItemClickListener() {
             @Override
             public void onChangeFavoriteStateClick(int position) {
-                mPresenter.changeFavoriteState(position);
+                presenter.changeFavoriteState(position);
             }
 
             @Override
             public void OnRecyclerItemClick(int position) {
-                mPresenter.openNewsDetails(position);
+                presenter.openNewsDetails(position);
             }
         });
-
-        return root;
     }
 
     @Override
-    public void changeFavoriteIcon(ImageButton button) {
-        if (mPresenter.getShowFavorite()) {
-            button.setImageDrawable(IconicStorage.getYellowStarBorderless(getContext()));
+    public void showNews() {
+        Log.d(TAG, "showNews: " + presenter.getNews());
+        setRecyclerViewOnDisplay();
+        List<NewsItem> newsItems = presenter.getNews();
+        adapter.setItems(newsItems);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void setFavoriteIcon() {
+        if (presenter.getShowFavorite()) {
+            favoriteNewsButton.setImageDrawable(IconicStorage.getYellowStarBorderless(context));
         } else {
-            button.setImageDrawable(IconicStorage.getWhiteStarBorderless(getContext()));
+            favoriteNewsButton.setImageDrawable(IconicStorage.getWhiteStarBorderless(context));
         }
     }
 
@@ -107,9 +144,17 @@ public class NewsFeedFragment extends Fragment implements NewsFeedContract.View 
         emptyView.setVisibility(View.GONE);
     }
 
+    private void fillFragmentByView() {
+        if (presenter.getNews().isEmpty()) {
+            setEmptyViewOnDisplay();
+        } else {
+            setRecyclerViewOnDisplay();
+        }
+    }
+
     @Override
     public void updateCategoryNameOnDescription() {
-        descriptionView.setText(mPresenter.getDisplayDescriptionText());
+        descriptionView.setText(presenter.getDisplayDescriptionText());
     }
 
     @Nullable
@@ -123,4 +168,18 @@ public class NewsFeedFragment extends Fragment implements NewsFeedContract.View 
         return adapter;
     }
 
+    void onCategorySelected(Category selectCategory) {
+        presenter.setCategory(selectCategory);
+//        performCall();
+        updateCategoryNameOnDescription();
+        updateNews();
+        fillFragmentByView();
+    }
+
+    private void updateNews(){
+        Log.d(TAG, "updateNews: ");
+        presenter.loadNews();
+        adapter.setItems(presenter.getNews());
+        adapter.notifyDataSetChanged();
+    }
 }
--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedPresenter.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedPresenter.java
@@ -1,15 +1,29 @@
 package ru.vssemikoz.newsfeed.newsfeed;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
+import android.widget.ProgressBar;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import ru.vssemikoz.newsfeed.MainApplication;
+import ru.vssemikoz.newsfeed.R;
 import ru.vssemikoz.newsfeed.adapters.NewsFeedAdapter;
 import ru.vssemikoz.newsfeed.dao.NewsItemDAO;
 import ru.vssemikoz.newsfeed.models.Category;
+import ru.vssemikoz.newsfeed.models.NewsApiResponse;
+import ru.vssemikoz.newsfeed.models.NewsApiResponseItem;
 import ru.vssemikoz.newsfeed.models.NewsItem;
+import ru.vssemikoz.newsfeed.storage.NewsApiRepository;
 import ru.vssemikoz.newsfeed.storage.NewsStorage;
 
 import static androidx.core.util.Preconditions.checkNotNull;
@@ -17,32 +31,40 @@ import static androidx.core.util.Preconditions.checkNotNull;
 public class NewsFeedPresenter implements NewsFeedContract.Presenter {
     private final NewsFeedContract.View newsFeedView;
 
+    private String TAG = NewsFeedPresenter.class.getName();
     private boolean showOnlyFavorite = false;
     private Category category = Category.ALL;
     private List<NewsItem> news;
+
+    private Callback<NewsApiResponse> callbackNewsItemList;
     private MainApplication mainApplication;
     private NewsStorage newsStorage;
 
-    public NewsFeedPresenter(NewsFeedContract.View tasksView){
+    NewsFeedPresenter(NewsFeedContract.View tasksView, MainApplication mainApplication){
         newsFeedView = checkNotNull(tasksView, "tasksView cannot be null!");
         newsFeedView.setPresenter(this);
+        this.mainApplication = mainApplication;
     }
+
     @Override
     public void start() {
-
-        mainApplication = (MainApplication) newsFeedView.getContext();
+        initNewsItemListCallback();
+        initNewsStorage();
+        Log.d(TAG, "start: ");
         loadNews();
+        newsFeedView.showNews();
     }
 
     @Override
     public void loadNews() {
-        // TODO: 16.03.2020
+        performCall();
         news = getNewsFromDB();
     }
 
     @Override
     public String getDisplayDescriptionText() {
-        return "Категория: " + Category.getDisplayName(category);
+        return mainApplication.getString(R.string.tv_category_string_prefix) +
+                Category.getDisplayName(category);
     }
 
     @Override
@@ -55,15 +77,14 @@ public class NewsFeedPresenter implements NewsFeedContract.Presenter {
         NewsItem item = news.get(position);
         item.invertFavoriteState();
         newsStorage.updateNews(item);
-        NewsFeedAdapter adapter = newsFeedView.getAdapter();
         if (!item.isFavorite() && showOnlyFavorite) {
             news.remove(position);
-            adapter.notifyItemRemoved(position);
+            newsFeedView.getAdapter().notifyItemRemoved(position);
             if (news.isEmpty()) {
                 newsFeedView.setEmptyViewOnDisplay();
             }
         } else {
-            adapter.notifyItemChanged(position);
+            newsFeedView.getAdapter().notifyItemChanged(position);
         }
     }
 
@@ -87,19 +108,28 @@ public class NewsFeedPresenter implements NewsFeedContract.Presenter {
         return this.showOnlyFavorite;
     }
 
+    @Override
+    public List<NewsItem> getNews() {
+        return news;
+    }
+
+    @Override
+    public void invertFavoriteState() {
+        showOnlyFavorite = !showOnlyFavorite;
+    }
+
     private void showNewsInBrowserByUrl(int position) {
         NewsItem item = news.get(position);
         String url = item.getUrl();
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setPackage("com.android.chrome");
-        // TODO: 16.03.2020
-//        try {
-//            getApplicationContext().startActivity(intent);
-//        } catch (ActivityNotFoundException ex) {
-//            intent.setPackage(null);
-//            getApplicationContext().startActivity(intent);
-//        }
+        try {
+            newsFeedView.getContext().startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            intent.setPackage(null);
+            newsFeedView.getContext().startActivity(intent);
+        }
     }
 
     private List<NewsItem> getNewsFromDB() {
@@ -109,5 +139,41 @@ public class NewsFeedPresenter implements NewsFeedContract.Presenter {
     private void initNewsStorage() {
         NewsItemDAO newsItemDAO = mainApplication.getNewsDataBase().newsItemDAO();
         newsStorage = new NewsStorage(newsItemDAO);
+    }
+
+    private void initNewsItemListCallback() {
+        callbackNewsItemList = new Callback<NewsApiResponse>() {
+            @Override
+            public void onResponse(@NotNull Call<NewsApiResponse> call, @NotNull Response<NewsApiResponse> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(TAG, "onResponse " + response.code());
+                    return;
+                }
+                newsStorage.insertUnique(getNewsItemListByResponse(response, category));
+                Log.d(TAG, "onResponse: ");
+                newsFeedView.getAdapter().setItems(news);
+                newsFeedView.getAdapter().notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<NewsApiResponse> call, @NotNull Throwable t) {
+                Log.d(TAG, "onFailure " + Objects.requireNonNull(t.getMessage()));
+            }
+        };
+    }
+
+    private void performCall() {
+        Log.d(TAG, "performCall: ");
+        NewsApiRepository newsApiRepository = new NewsApiRepository(mainApplication);
+        newsApiRepository.getNewsFromApi(category, callbackNewsItemList);
+    }
+
+    private List<NewsItem> getNewsItemListByResponse(Response<NewsApiResponse> response, Category category) {
+        List<NewsItem> news = new ArrayList<>();
+        List<NewsApiResponseItem> newsApiResponseItems = Objects.requireNonNull(response.body()).getNewsApiResponseItemList();
+        for (NewsApiResponseItem newsApiResponseItem : newsApiResponseItems) {
+            news.add(new NewsItem(newsApiResponseItem, category));
+        }
+        return news;
     }
 }
