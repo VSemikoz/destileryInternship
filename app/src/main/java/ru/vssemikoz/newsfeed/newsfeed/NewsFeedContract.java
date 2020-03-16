99200d5aef1af3d409911173248cc8cc4134d1d0
VSemikoz <slava.semik@yandex.ru>
2020-03-16 16:20:39 +0300

    add methods in contract

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedContract.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedContract.java
@@ -1,15 +1,47 @@
 package ru.vssemikoz.newsfeed.newsfeed;
 
+import android.content.Context;
+import android.widget.ImageButton;
+
 import ru.vssemikoz.newsfeed.BasePresenter;
 import ru.vssemikoz.newsfeed.BaseView;
+import ru.vssemikoz.newsfeed.adapters.NewsFeedAdapter;
+import ru.vssemikoz.newsfeed.models.Category;
 
 public interface NewsFeedContract {
 
     interface View extends BaseView<Presenter> {
 
+        void changeFavoriteIcon(ImageButton button);
+
+        void setEmptyViewOnDisplay();
+
+        void setRecyclerViewOnDisplay();
+
+        void updateCategoryNameOnDescription();
+
+        Context getContext();
+
+        NewsFeedAdapter getAdapter();
     }
 
     interface Presenter extends BasePresenter{
+
+        void loadNews();
+
+        String getDisplayDescriptionText();
+
+        void openNewsDetails(int position);
+
+        void changeFavoriteState(int position);
+
+        void setCategory(Category category);
+
+        Category getCategory();
+
+        void setShowFavorite(Boolean showOnlyFavorite);
+
+        Boolean getShowFavorite();
 
     }
 }
