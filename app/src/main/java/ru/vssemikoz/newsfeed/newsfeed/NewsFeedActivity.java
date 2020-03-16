95dc8c0b6cb39a3b064932948f3a5cbc82e79a89
VSemikoz <slava.semik@yandex.ru>
2020-03-16 14:33:27 +0300

    add carcass of NewsFeedActivity

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
@@ -1,14 +1,39 @@
 package ru.vssemikoz.newsfeed.newsfeed;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import ru.vssemikoz.newsfeed.R;
+
 public class NewsFeedActivity extends AppCompatActivity {
+
+    private NewsFeedPresenter newsFeedPresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_feed_fragment);
+
+        NewsFeedFragment newsFeedFragment =
+                (NewsFeedFragment) getSupportFragmentManager().findFragmentById(R.id.newsfeed_content_frame);
+        if (newsFeedFragment == null){
+            // TODO: 16.03.2020
+//            newsFeedFragment = NewsFeedFragment.newInstance();
+//            ActivityUtils.addFragmentToActivity(
+//                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
+
+        // TODO: 16.03.2020
+//        newsFeedPresenter = NewsFeedPresenter(
+//                Injection.provideTasksRepository(getApplicationContext()), tasksFragment);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        // TODO: 16.03.2020
     }
 }
