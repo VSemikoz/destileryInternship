76039475e8e00f7f28a4d4335b936e79c92570de
VSemikoz <slava.semik@yandex.ru>
2020-03-16 21:17:53 +0300

    add onCategorySelected() override method

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
@@ -2,35 +2,43 @@ package ru.vssemikoz.newsfeed.newsfeed;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import ru.vssemikoz.newsfeed.MainApplication;
 import ru.vssemikoz.newsfeed.R;
+import ru.vssemikoz.newsfeed.dialogs.PickCategoryDialog;
 import ru.vssemikoz.newsfeed.models.Category;
 import ru.vssemikoz.newsfeed.utils.TypeConverters.ActivityUtils;
 
-public class NewsFeedActivity extends AppCompatActivity {
-
+public class NewsFeedActivity extends AppCompatActivity implements PickCategoryDialog.OnCategorySelectedListener{
+    private String TAG = NewsFeedActivity.class.getName();
     private NewsFeedPresenter newsFeedPresenter;
+    private NewsFeedFragment newsFeedFragment;
     private String CURRENT_CATEGORY = "CURRENT_CATEGORY";
     private String CURRENT_SHOW_FAVORITE = "CURRENT_SHOW_FAVORITE";
+    private MainApplication mainApplication;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_feed_fragment);
+        setContentView(R.layout.newsfeed_act);
+        mainApplication = (MainApplication) getApplicationContext();
 
-        NewsFeedFragment newsFeedFragment =
+        newsFeedFragment =
                 (NewsFeedFragment) getSupportFragmentManager().findFragmentById(R.id.newsfeed_content_frame);
         if (newsFeedFragment == null) {
+
             newsFeedFragment = NewsFeedFragment.newInstance();
             ActivityUtils.addFragmentToActivity(
                     getSupportFragmentManager(), newsFeedFragment, R.id.newsfeed_content_frame);
         }
 
-        newsFeedPresenter = new NewsFeedPresenter(newsFeedFragment);
+        newsFeedPresenter = new NewsFeedPresenter(newsFeedFragment, mainApplication);
 
         if (savedInstanceState != null) {
             Category category =
@@ -45,5 +53,10 @@ public class NewsFeedActivity extends AppCompatActivity {
     public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
         outState.putSerializable(CURRENT_CATEGORY, newsFeedPresenter.getCategory());
         outState.getBoolean(CURRENT_SHOW_FAVORITE, newsFeedPresenter.getShowFavorite());
+    }
+
+    @Override
+    public void onCategorySelected(Category selectCategory) {
+        newsFeedFragment.onCategorySelected(selectCategory);
     }
 }
