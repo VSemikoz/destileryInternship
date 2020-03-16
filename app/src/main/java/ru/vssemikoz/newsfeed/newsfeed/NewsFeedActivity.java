70d2ebc2a703f85bd8ca37c77538933e8714f833
VSemikoz <slava.semik@yandex.ru>
2020-03-16 16:21:21 +0300

    add activity functionality

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
@@ -8,10 +8,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ru.vssemikoz.newsfeed.R;
+import ru.vssemikoz.newsfeed.models.Category;
+import ru.vssemikoz.newsfeed.utils.TypeConverters.ActivityUtils;
 
 public class NewsFeedActivity extends AppCompatActivity {
 
     private NewsFeedPresenter newsFeedPresenter;
+    private String CURRENT_CATEGORY = "CURRENT_CATEGORY";
+    private String CURRENT_SHOW_FAVORITE = "CURRENT_SHOW_FAVORITE";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,20 +24,26 @@ public class NewsFeedActivity extends AppCompatActivity {
 
         NewsFeedFragment newsFeedFragment =
                 (NewsFeedFragment) getSupportFragmentManager().findFragmentById(R.id.newsfeed_content_frame);
-        if (newsFeedFragment == null){
-            // TODO: 16.03.2020
+        if (newsFeedFragment == null) {
             newsFeedFragment = NewsFeedFragment.newInstance();
-//            ActivityUtils.addFragmentToActivity(
-//                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), newsFeedFragment, R.id.newsfeed_content_frame);
         }
 
-        // TODO: 16.03.2020
-//        newsFeedPresenter = NewsFeedPresenter(
-//                Injection.provideTasksRepository(getApplicationContext()), tasksFragment);
+        newsFeedPresenter = new NewsFeedPresenter(newsFeedFragment);
+
+        if (savedInstanceState != null) {
+            Category category =
+                    (Category) savedInstanceState.getSerializable(CURRENT_CATEGORY);
+            Boolean showFavoriteNews = (Boolean) savedInstanceState.getBoolean(CURRENT_SHOW_FAVORITE);
+            newsFeedPresenter.setShowFavorite(showFavoriteNews);
+            newsFeedPresenter.setCategory(category);
+        }
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        // TODO: 16.03.2020
+        outState.putSerializable(CURRENT_CATEGORY, newsFeedPresenter.getCategory());
+        outState.getBoolean(CURRENT_SHOW_FAVORITE, newsFeedPresenter.getShowFavorite());
     }
 }
