2df299684f538021b47e7247a606aa6d4dfca027
VSemikoz <slava.semik@yandex.ru>
2020-03-16 14:15:54 +0300

    add NewsFeedPresenter and NewsFeedFragment

---
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedFragment.java
@@ -0,0 +1,10 @@
+package ru.vssemikoz.newsfeed.newsfeed;
+
+import androidx.fragment.app.Fragment;
+
+public class NewsFeedFragment extends Fragment implements NewsFeedContract.View {
+    @Override
+    public void setPresenter(NewsFeedContract.Presenter presenter) {
+
+    }
+}
---
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedPresenter.java
@@ -0,0 +1,8 @@
+package ru.vssemikoz.newsfeed.newsfeed;
+
+public class NewsFeedPresenter implements NewsFeedContract.Presenter {
+    @Override
+    public void start() {
+
+    }
+}
