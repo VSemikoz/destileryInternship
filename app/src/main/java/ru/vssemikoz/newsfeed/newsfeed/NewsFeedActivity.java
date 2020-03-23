2521c4970e7bc38eed568198cb725be39c1dc8d8
VSemikoz <slava.semik@yandex.ru>
2020-03-23 14:07:02 +0300

    delete log string

--- app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/newsfeed/NewsFeedActivity.java
@@ -9,7 +9,6 @@ import ru.vssemikoz.newsfeed.R;
 import ru.vssemikoz.newsfeed.utils.ActivityUtils;
 
 public class NewsFeedActivity extends AppCompatActivity {
-    private String TAG = NewsFeedActivity.class.getName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
