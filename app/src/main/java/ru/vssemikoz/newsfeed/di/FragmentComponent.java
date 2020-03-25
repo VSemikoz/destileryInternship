6024a4d816bd9c6a2a1f54130258e8254dc94e0d
VSemikoz <slava.semik@yandex.ru>
2020-03-25 17:41:14 +0300

    code refactor

--- app/src/main/java/ru/vssemikoz/newsfeed/di/FragmentComponent.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/FragmentComponent.java
@@ -3,10 +3,7 @@ package ru.vssemikoz.newsfeed.di;
 import dagger.Subcomponent;
 import ru.vssemikoz.newsfeed.newsfeed.NewsFeedFragment;
 
-@Subcomponent(modules = {AdapterModule.class,
-                        PresenterModule.class,
-                        ApiRepositoryModule.class,
-                        StorageModule.class,})
+@Subcomponent(modules = {AdapterModule.class, PresenterModule.class, ApiRepositoryModule.class, StorageModule.class,})
 public interface FragmentComponent {
     void inject(NewsFeedFragment fragment);
 }
