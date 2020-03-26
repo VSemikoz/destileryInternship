d98fd6244a6c0852275fdcef63e43f70e97c48f0
VSemikoz <slava.semik@yandex.ru>
2020-03-26 19:27:12 +0300

    provide baseUrl in NetworkModule by gradle

--- app/src/main/java/ru/vssemikoz/newsfeed/di/NetworkModule.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/NetworkModule.java
@@ -4,21 +4,16 @@ import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import ru.vssemikoz.newsfeed.AppConfig;
 import ru.vssemikoz.newsfeed.api.NewsApi;
 
 @Module
 public class NetworkModule {
-    // TODO: 26.03.2020 into gradle config
-    private String baseUrl;
-
-    public NetworkModule(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
 
     @Provides
-    Retrofit provideRetrofit() {
+    Retrofit provideRetrofit(AppConfig config) {
         return new Retrofit.Builder()
-                .baseUrl(baseUrl)
+                .baseUrl(config.getBaseUrl())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
