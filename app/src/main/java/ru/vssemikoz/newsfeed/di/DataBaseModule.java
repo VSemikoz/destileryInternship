376e8506fae8530a32069d922f80ecbc04a6a2dd
VSemikoz <slava.semik@yandex.ru>
2020-03-26 17:20:00 +0300

    send context into provideDataBase instead mainApplication

--- app/src/main/java/ru/vssemikoz/newsfeed/di/DataBaseModule.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/DataBaseModule.java
@@ -1,18 +1,19 @@
 package ru.vssemikoz.newsfeed.di;
 
+import android.content.Context;
+
 import androidx.room.Room;
 
 import dagger.Module;
 import dagger.Provides;
-import ru.vssemikoz.newsfeed.MainApplication;
 import ru.vssemikoz.newsfeed.database.NewsAppDataBase;
 
 @Module
 public class DataBaseModule {
 
     @Provides
-    NewsAppDataBase provideDataBase(MainApplication application){
-        return Room.databaseBuilder(application.getApplicationContext(),
+    NewsAppDataBase provideDataBase(Context context){
+        return Room.databaseBuilder(context,
                 NewsAppDataBase.class, "news_data_base")
                 .allowMainThreadQueries()
                 .fallbackToDestructiveMigration()
