ff6bc8ae7f39528aee2005659d7406b7ce8072de
VSemikoz <slava.semik@yandex.ru>
2020-03-26 17:18:18 +0300

    remove cements lines

--- app/src/main/java/ru/vssemikoz/newsfeed/di/ApplicationComponent.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/ApplicationComponent.java
@@ -3,8 +3,6 @@ package ru.vssemikoz.newsfeed.di;
 import dagger.Component;
 import ru.vssemikoz.newsfeed.MainApplication;
 
-//dependencies force call fragmentComponent() in building
-//@Component(dependencies = {FragmentComponent.class}, modules = {ApplicationModule.class, PresenterModule.class})
 @Component(modules = {ApplicationModule.class,
                         NetworkModule.class,
                         DataBaseModule.class})
