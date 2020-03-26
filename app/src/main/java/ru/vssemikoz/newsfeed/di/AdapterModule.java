a68091a41499f95a4726e8680c672f7d5cd3149d
VSemikoz <slava.semik@yandex.ru>
2020-03-26 14:04:46 +0300

    send context into provider as parameter instead mainApplication

--- app/src/main/java/ru/vssemikoz/newsfeed/di/AdapterModule.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/AdapterModule.java
@@ -1,15 +1,16 @@
 package ru.vssemikoz.newsfeed.di;
 
+import android.content.Context;
+
 import dagger.Module;
 import dagger.Provides;
-import ru.vssemikoz.newsfeed.MainApplication;
 import ru.vssemikoz.newsfeed.adapters.NewsFeedAdapter;
 
 @Module
 public class AdapterModule {
 
     @Provides
-    NewsFeedAdapter provideAdapter(MainApplication application){
-        return new NewsFeedAdapter(application.getApplicationContext());
+    NewsFeedAdapter provideAdapter(Context context){
+        return new NewsFeedAdapter(context);
     }
 }
