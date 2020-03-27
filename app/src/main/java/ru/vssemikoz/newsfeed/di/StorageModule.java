acd64dc64e32e196fed5af6cd8414fbe58c52bd7
VSemikoz <slava.semik@yandex.ru>
2020-03-27 14:46:37 +0300

    rename NewsStorage -> LocalNewsStorage

--- app/src/main/java/ru/vssemikoz/newsfeed/di/StorageModule.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/di/StorageModule.java
@@ -2,13 +2,13 @@ package ru.vssemikoz.newsfeed.di;
 
 import dagger.Module;
 import dagger.Provides;
-import ru.vssemikoz.newsfeed.data.NewsStorage;
+import ru.vssemikoz.newsfeed.data.LocalNewsStorage;
 import ru.vssemikoz.newsfeed.database.NewsAppDataBase;
 
 @Module
 public class StorageModule {
     @Provides
-    NewsStorage provideStorage(NewsAppDataBase dataBase){
-        return new NewsStorage(dataBase);
+    LocalNewsStorage provideStorage(NewsAppDataBase dataBase){
+        return new LocalNewsStorage(dataBase);
     }
 }
