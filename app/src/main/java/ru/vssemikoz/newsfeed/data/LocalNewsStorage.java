66fc449ad94e14a95cd4bd85a0a83506bd8039f0
VSemikoz <slava.semik@yandex.ru>
2020-03-30 15:32:05 +0300

    add call of async task in LocalNewsStorage

--- app/src/main/java/ru/vssemikoz/newsfeed/data/LocalNewsStorage.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/data/LocalNewsStorage.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import ru.vssemikoz.newsfeed.Tasks.DeleteTask;
+import ru.vssemikoz.newsfeed.Tasks.GetFilteredTask;
+import ru.vssemikoz.newsfeed.Tasks.InsertUniqueTask;
+import ru.vssemikoz.newsfeed.Tasks.UpdateTask;
 import ru.vssemikoz.newsfeed.dao.NewsItemDAO;
 import ru.vssemikoz.newsfeed.database.NewsAppDataBase;
 import ru.vssemikoz.newsfeed.models.Category;
@@ -12,38 +16,42 @@ import ru.vssemikoz.newsfeed.models.NewsItem;
 public class LocalNewsStorage implements NewsStorage {
     private NewsItemDAO newsItemDAO;
 
+    private void setResultNews(List<NewsItem> resultNews, RequestListener listener) {
+        if (resultNews ==null || resultNews.isEmpty()){
+            listener.onRequestFailure();
+        } else {
+            listener.onRequestSuccess(resultNews);
+        }
+    }
+
     @Inject
     public LocalNewsStorage(NewsAppDataBase dataBase) {
         this.newsItemDAO = dataBase.newsItemDAO();
     }
 
     @Override
-    public List<NewsItem> getFiltered(boolean favoriteNewsState, Category category) {
-        if (favoriteNewsState) {
-            if (category == Category.ALL) {
-                return newsItemDAO.getFavoriteNews();
-            } else {
-                return newsItemDAO.getFavoriteNewsByCategory(Category.getCategoryName(category));
-            }
-        }
-        if (category == Category.ALL) {
-            return newsItemDAO.getAll();
-        }
-        return newsItemDAO.getNewsByCategory(Category.getCategoryName(category));
+    public void getFiltered(boolean favoriteNewsState, Category category,  RequestListener listener) {
+        GetFilteredTask task = new GetFilteredTask(favoriteNewsState, category, newsItemDAO, items -> setResultNews(items, listener));
+        task.execute();
     }
 
     @Override
     public void deleteAll() {
-        newsItemDAO.deleteAll();
+        DeleteTask task = new DeleteTask(newsItemDAO);
+        task.execute();
     }
 
     @Override
     public void updateItem(NewsItem item) {
-        newsItemDAO.update(item);
+        UpdateTask task =  new UpdateTask(newsItemDAO, item);
+        task.execute();
     }
 
     @Override
     public void insertUnique(List<NewsItem> newsItems) {
-        newsItemDAO.insertUnique(newsItems);
+        InsertUniqueTask task = new InsertUniqueTask(newsItemDAO, newsItems);
+        task.execute();
     }
+
 }
+
