1c9e43c41892a2dcb74f069558ba9d77816ec2d6
VSemikoz <slava.semik@yandex.ru>
2020-04-02 15:27:21 +0300

    change NewsMapper signature; change ApiResponseMapper implementation

--- app/src/main/java/ru/vssemikoz/newsfeed/data/mappers/ApiResponseMapper.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/data/mappers/ApiResponseMapper.java
@@ -2,27 +2,28 @@ package ru.vssemikoz.newsfeed.data.mappers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.inject.Inject;
 
 import retrofit2.Response;
-import ru.vssemikoz.newsfeed.models.Filter;
+import ru.vssemikoz.newsfeed.models.Category;
 import ru.vssemikoz.newsfeed.models.NewsApiResponse;
 import ru.vssemikoz.newsfeed.models.NewsApiResponseItem;
 import ru.vssemikoz.newsfeed.models.NewsItem;
+import ru.vssemikoz.newsfeed.models.Params;
 
-public class ApiResponseMapper implements NewsMapper<Response<NewsApiResponse>, Filter> {
+public class ApiResponseMapper implements NewsMapper<Params> {
     @Inject
     public ApiResponseMapper() {
     }
 
     @Override
-    public List<NewsItem> map(Response<NewsApiResponse> mapItems, Filter params) {
+    public List<NewsItem> map(Response<NewsApiResponse> response, Params params) {
         List<NewsItem> news = new ArrayList<>();
-        List<NewsApiResponseItem> responseItems = mapItems.body().getNewsApiResponseItemList();
+        Category category = params.getFilter().getCategory();
+        List<NewsApiResponseItem> responseItems = response.body().getNewsApiResponseItemList();
         for (NewsApiResponseItem newsApiResponseItem : responseItems) {
-            news.add(new NewsItem(newsApiResponseItem, params.getCategory()));
+            news.add(new NewsItem(newsApiResponseItem, category));
         }
         return news;
     }
