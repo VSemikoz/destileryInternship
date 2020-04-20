488c4c0e4bdb80254c187a972f6be500f9017c5a
VSemikoz <slava.semik@yandex.ru>
2020-04-20 16:20:38 +0300

    change return type

--- app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
@@ -1,13 +1,17 @@
 package ru.vssemikoz.newsfeed.api;
 
-import retrofit2.Call;
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import ru.vssemikoz.newsfeed.models.NewsApiResponse;
+import ru.vssemikoz.newsfeed.models.NewsItem;
 
 public interface NewsApi {
     @GET("/v2/top-headlines")
-    Call<NewsApiResponse> getNews(@Query("country") String country,
-                                  @Query("category") String category,
-                                  @Query("apiKey") String key);
+    Response<NewsApiResponse> getNews(@Query("country") String country,
+                                             @Query("category") String category,
+                                             @Query("apiKey") String key);
 }
