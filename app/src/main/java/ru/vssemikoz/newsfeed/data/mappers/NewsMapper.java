86f6bd076c0adbf141bd194bfc8fe332eb031a13
VSemikoz <slava.semik@yandex.ru>
2020-04-20 20:35:29 +0300

    change arguments mapper type

--- app/src/main/java/ru/vssemikoz/newsfeed/data/mappers/NewsMapper.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/data/mappers/NewsMapper.java
@@ -2,11 +2,10 @@ package ru.vssemikoz.newsfeed.data.mappers;
 
 import java.util.List;
 
-import retrofit2.Response;
 import ru.vssemikoz.newsfeed.models.NewsApiResponse;
 import ru.vssemikoz.newsfeed.models.NewsItem;
 import ru.vssemikoz.newsfeed.models.NewsFeedParams;
 
 public interface NewsMapper {
-    List<NewsItem> map(Response<NewsApiResponse> response, NewsFeedParams params);
+    List<NewsItem> map(NewsApiResponse response, NewsFeedParams params);
 }
