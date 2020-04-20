9f653e51d6e190b6ff43adbab26708bc8ad202cd
VSemikoz <slava.semik@yandex.ru>
2020-04-20 20:34:32 +0300

    change return type Response<NewsApiResponse> -> Single<NewsApiResponse>

--- app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/api/NewsApi.java
@@ -1,17 +1,13 @@
 package ru.vssemikoz.newsfeed.api;
 
-import java.util.List;
-
 import io.reactivex.rxjava3.core.Single;
-import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import ru.vssemikoz.newsfeed.models.NewsApiResponse;
-import ru.vssemikoz.newsfeed.models.NewsItem;
 
 public interface NewsApi {
     @GET("/v2/top-headlines")
-    Response<NewsApiResponse> getNews(@Query("country") String country,
-                                             @Query("category") String category,
-                                             @Query("apiKey") String key);
+    Single<NewsApiResponse> getNews(@Query("country") String country,
+                                         @Query("category") String category,
+                                         @Query("apiKey") String key);
 }
