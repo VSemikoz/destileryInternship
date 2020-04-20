a5339ae55c5192a94c4342aa98c1eea6b53f6067
VSemikoz <slava.semik@yandex.ru>
2020-04-20 16:21:48 +0300

    use case refactor

--- app/src/main/java/ru/vssemikoz/newsfeed/usecases/UpdateStorageUseCase.java
+++ app/src/main/java/ru/vssemikoz/newsfeed/usecases/UpdateStorageUseCase.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.rxjava3.core.Single;
 import retrofit2.Response;
 import ru.vssemikoz.newsfeed.data.NewsRepository;
 import ru.vssemikoz.newsfeed.data.NewsStorage;
-import ru.vssemikoz.newsfeed.data.RemoteNewsRepository;
 import ru.vssemikoz.newsfeed.data.mappers.NewsMapper;
 import ru.vssemikoz.newsfeed.models.NewsApiResponse;
 import ru.vssemikoz.newsfeed.models.NewsFeedParams;
 import ru.vssemikoz.newsfeed.models.NewsItem;
 
-public class UpdateStorageUseCase implements BaseUseCase<Void, NewsFeedParams> {
+public class UpdateStorageUseCase implements BaseUseCase<Single<List<NewsItem>>, NewsFeedParams> {
     private static final String TAG = UpdateStorageUseCase.class.getName();
     @Inject
     NewsStorage newsStorage;
@@ -29,29 +29,33 @@ public class UpdateStorageUseCase implements BaseUseCase<Void, NewsFeedParams> {
     }
 
     @Override
-    public Void run(NewsFeedParams params) {
-        repository.getNewsFiltered(params.getFilter().getCategory(), getListener(params));
-        return null;
+    public Single<List<NewsItem>> run(NewsFeedParams params) {
+//        repository.getNewsFiltered(params.getFilter().getCategory(), getListener(params));
+//        return null;
+
+        return repository.getNewsFiltered(params)
+                .doOnSubscribe(news -> newsStorage.insertUnique(news))
+//                .subscribe(news -> newsStorage.insertUnique(news))
     }
 
-    public RemoteNewsRepository.RequestListener getListener(NewsFeedParams params){
-        NewsFeedParams.RequestListener listener = params.getListener();
-        return new NewsRepository.RequestListener() {
-            @Override
-            public void onRequestSuccess(Response<NewsApiResponse> response) {
-                if (!response.isSuccessful()) {
-                    Log.d(TAG, "onResponse " + response.code());
-                    return;
-                }
-                List<NewsItem> news = mapper.map(response, params);
-                newsStorage.insertUnique(news);
-                listener.onRequestSuccess(news);
-            }
-
-            @Override
-            public void onRequestFailure(Throwable t) {
-                listener.onRequestFailure();
-            }
-        };
-    }
+//    public RemoteNewsRepository.RequestListener getListener(NewsFeedParams params) {
+//        NewsFeedParams.RequestListener listener = params.getListener();
+//        return new NewsRepository.RequestListener() {
+//            @Override
+//            public void onRequestSuccess(Response<NewsApiResponse> response) {
+//                if (!response.isSuccessful()) {
+//                    Log.d(TAG, "onResponse " + response.code());
+//                    return;
+//                }
+//                List<NewsItem> news = mapper.map(response, params);
+//                newsStorage.insertUnique(news);
+//                listener.onRequestSuccess(news);
+//            }
+//
+//            @Override
+//            public void onRequestFailure(Throwable t) {
+//                listener.onRequestFailure();
+//            }
+//        };
+//    }
 }
